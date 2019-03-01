1ccc38b878184edced69f37ff0ef3b276b2eebfb
MrFuku <cardbed5035@gmail.com>
2019-03-01 23:47:33 +0900

    home layout test build

--- Gemfile
+++ Gemfile
@@ -43,8 +43,10 @@ group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'capybara', '>= 2.15'
   gem 'selenium-webdriver'
+  gem 'chromedriver-helper'
   gem "rspec-rails", "~> 3.7"
   gem "factory_bot_rails", "~> 4.11"
+  gem 'rails-controller-testing'
 end
 
 group :development do
--- spec/requests/static_pages_controller_spec.rb
+++ spec/requests/static_pages_controller_spec.rb
@@ -1,8 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe StaticPagesController, type: :controller do
-  render_views
-
+RSpec.describe "StaticPages", type: :request do
   let(:base_title) { "Ruby on Rails Tutorial Sample App" }
   let(:title) { "#{page_title}Ruby on Rails Tutorial Sample App" }
 
@@ -16,25 +14,25 @@ RSpec.describe StaticPagesController, type: :controller do
   end
 
   describe "GET #home" do
-    before { get :home }
+    before { get root_path }
     let(:page_title) { "" }
     it_behaves_like "レスポンス、タイトル確認"
   end
 
   describe "GET #help" do
-    before { get :help }
+    before { get help_path }
     let(:page_title) { "Help | " }
     it_behaves_like "レスポンス、タイトル確認"
   end
 
   describe "GET #about" do
-    before { get :about }
+    before { get about_path }
     let(:page_title) { "About | " }
     it_behaves_like "レスポンス、タイトル確認"
   end
 
   describe "GET #contact" do
-    before { get :contact }
+    before { get contact_path }
     let(:page_title) { "Contact | " }
     it_behaves_like "レスポンス、タイトル確認"
   end
